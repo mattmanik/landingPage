0f8fd5b52be51b310fdd05415045f5ef4e07bc75
Mani kandan <mani22k@hotmail.com>
2021-06-29 00:03:38 +0530

    Just setting things up

--- src/App.js
+++ src/App.js
@@ -8,11 +8,7 @@ function App() {
       <header className="App-header">
       <Alert>This is a alert message</Alert>
        <Button>Bootstrap</Button>
-       <Button>Bootstrap</Button>
-       <Button>Bootstrap</Button>
-       <Button>Bootstrap</Button>
       </header>
-      <Row className='row'><p>Hellloooooo</p></Row>
       <Row className='row'><p>Hellloooooo</p></Row>
     </div>
   );
